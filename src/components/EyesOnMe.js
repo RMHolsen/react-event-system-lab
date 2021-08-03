3a5a0cedc60b3fbbe80c9d5bd5795e235b186fed
rmholsen <jaguar.kitty@gmail.com>
2021-08-03 12:53:42 -0400

    Completed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+export default class EyesOnMe extends React.Component {
+    render() {
+        return(
+            <button onFocus={this.focusHandler} onBlur={this.blurHandler}>
+                Focus Button
+            </button>
+        )
+    }
+
+    focusHandler = () => {
+        console.log('Good!');
+    }
+
+    blurHandler = () => {
+        console.log('Hey! Eyes on me!');
+    }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
 // Code Keypad Component Here
+import React from 'react';
+// maybe look up when to use import React { Component } and when to use import React?? 
+
+export default class Keypad extends React.Component {
+    // render an input (with the right type??) oh input tag with the right type
+    render() {
+        return(
+            <div><input type="password" onKeyUp={this.handleKeyUp}></input></div>
+        )
+    }
+
+    handleKeyUp = () => {
+        console.log('Entering password...')
+    }
+}
